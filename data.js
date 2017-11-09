2845fbb4a2a55da059e24fd1c6fedfc74468b01f
Umar Thariq <umar.thariq@gdn-commerce.com>
2017-11-09 15:34:28 +0530

    all the sub pages are added

--- data.js
+++ data.js
@@ -5,22 +5,22 @@ var projectList = [{
 },
 {
   id: 2,
-  header: 'Blibli.com product detail page - Revamp. - [Angular JS, Javascript (ES6), SCSS, CSS.]',
+  header: 'Blibli.com product detail page revamp - [Angular JS, Javascript (ES6), SCSS, CSS.]',
   desc: 'Blibli.com is the E-Commerce platform in Indonesia. Worked on product detail page revamp, which provides the detailed information of the product to the user. '
 },
 {
   id: 3,
-  header: 'Transport Management Systems [React JS, Javascript (ES6), SCSS, CSS, Webpack, Babel.]',
+  header: 'Transport Management Systems - [React JS, Javascript (ES6), SCSS, CSS, Webpack, Babel.]',
   desc: 'TMS is the delivery management system that helps Blibli.com delivers its order in effective manner.'
 },
 {
   id: 4,
-  header: 'Seller UI Platform for ZOPPER (E-Commerce) [Angular JS, Javascript, SCSS, CSS, Webpack.]',
+  header: 'Seller UI Platform for ZOPPER - [Angular JS, Javascript, SCSS, CSS, Webpack.]',
   desc: 'Seller UI is the platform where the Sellers can manage their virtual stores and track their business.'
 },
 {
   id: 5,
-  header: 'Relationship Manager’s UI ZOPPER (E-Commerce) [Angular JS, Javascript, SCSS, CSS, Webpack.]',
+  header: 'Relationship Manager’s UI ZOPPER - [Angular JS, Javascript, SCSS, CSS, Webpack.]',
   desc: 'RM Dashboard used to track the relationship managers performance, collobrate among them and motivate them to achieve their future targets. '
 },
 {
