b9aeb771f32b62f47c72706d6bbe01856a330df1
Umar Thariq <umar.thariq@gdn-commerce.com>
2017-11-10 13:05:37 +0530

    some config changes for heroku

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var projectList = require('./data.js').projectList;
 // var bodyParser = require('body-parser');
 debugger;
 var app = express();
-app.set('port', (process.env.PORT || 5000));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 // var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.set('view engine', 'ejs');
