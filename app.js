dfb429cf452e822319c26842e05b2bc8865e467d
Umar Thariq <umar.thariq@gdn-commerce.com>
2017-11-13 20:57:19 +0530

    font ttfs are minimized

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.use(bodyParser.urlencoded({extended: true})); // to support URL-encoded bodi
 app.set('view engine', 'ejs');
 
 app.use('/images', express.static(path.join(__dirname,'/dist/images/')));
+app.use('/styles/fonts', express.static(path.join(__dirname,'/dist/fonts/')));
 app.use('/styles', express.static('styles'));
 app.use('/scripts', express.static('scripts'));
-app.use('/fonts', express.static('fonts'));
 
 app.get('/', (req, res)=>{
   res.render('index',{projectList});
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
+const fontmin = require('gulp-fontmin');
 
-gulp.task('default', () =>
+gulp.task('imagemin', () =>
     gulp.src(__dirname+'/images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/images'))
+);
+
+gulp.task('fontmin', () =>
+    gulp.src(__dirname+'/styles/myfonts/*.ttf')
+        .pipe(fontmin())
+        .pipe(gulp.dest('dist/fonts'))
 );
