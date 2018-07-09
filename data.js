4567b4465af3336c59291be5b300bfbfd98e6913
Umar Thariq <umar.thariq@gdn-commerce.com>
2018-07-09 19:06:20 +0530

    gulp changes

--- data.js
+++ data.js
@@ -1,38 +1,46 @@
 var projectList = [{
   id: 1,
+  header: 'LTS (Lead Tracking System) - [React JS, Javascript (ES6), SCSS, CSS3, Babel, Webpack.]',
+  desc: 'This dashboard helps to automate the management of the enquiries and execution of orders in BLIBLI B2B Offline Operations.'
+},{
+  id: 2,
+  header: 'Gamification (Employee Engagement) - [Angular JS, Node JS, Javascript (ES5), SCSS, CSS3, Gulp.]',
+  desc: 'This feature helps to improve the engagement of the internal employees on organisation oriented activities and also motivate them to complete their work very efficient manner.'
+},{
+  id: 3,
   header: 'Campaign Center - [React JS, Javascript (ES6), SCSS, CSS3, Babel, Webpack.]',
   desc: 'Campaign centre is the dashboard which helps to provide single window solution for creating and monitoring the promotions in Blibli.com.'
 },
 {
-  id: 2,
+  id: 4,
   header: 'Blibli.com product detail page revamp - [Angular JS, Javascript (ES6), SCSS, CSS.]',
   desc: 'Blibli.com is the E-Commerce platform in Indonesia. Worked on product detail page revamp, which provides the detailed information of the product to the user. '
 },
 {
-  id: 3,
+  id: 5,
   header: 'Transport Management Systems - [React JS, Javascript (ES6), SCSS, CSS, Webpack, Babel.]',
   desc: 'TMS is the delivery management system that helps Blibli.com delivers its order in effective manner.'
 },
 {
-  id: 4,
+  id: 6,
   header: 'Seller UI Platform for ZOPPER - [Angular JS, Javascript, SCSS, CSS, Webpack.]',
   desc: 'Seller UI is the platform where the Sellers can manage their virtual stores and track their business.'
 },
 {
-  id: 5,
+  id: 7,
   header: 'Relationship Manager’s UI ZOPPER - [Angular JS, Javascript, SCSS, CSS, Webpack.]',
   desc: 'RM Dashboard used to track the relationship managers performance, collobrate among them and motivate them to achieve their future targets. '
 },
 {
-  id: 6,
+  id: 8,
   header: 'Vellore Online Information System - [HTML, CSS, Javascript, BootStrap.]',
   desc: 'VOIS is an online web-portal which contains, organized collection of information about Vellore.'
 },
 {
-  id: 7,
+  id: 9,
   header: 'Cyber Encryption (Web Application) - [HTML, CSS, Javascript, BootStrap, MySQL.]',
   desc: 'Created the Web portal to sharing the secret informations among the group of people using AES(128 bit) and BlowFish algorithms .'
-},
+}
 ];
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const fontmin = require('gulp-fontmin');
 const cleanCss = require('gulp-clean-css');
+const gulpSass = require('gulp-sass');
 
 gulp.task('imagemin', () =>
     gulp.src(__dirname+'/images/*')
@@ -15,8 +16,17 @@ gulp.task('fontmin', () =>
         .pipe(gulp.dest('dist/fonts'))
 );
 
-gulp.task('minify-css', ()=>{
-    return gulp.src(__dirname+'/styles/*.css')
-    .pipe(cleanCss({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist/styles'));
+gulp.task('sass', ()=>{
+    return gulp.src(__dirname+'/styles/*.scss')
+    .pipe(gulpSass())
+    .pipe(gulp.dest('dist/styles/'));
 });
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./styles/*.scss', ['sass']);
+});
+
+gulp.task('tasks', function(){
+    gulp.start('imagemin', 'fontmin', 'sass', 'sass:watch')
+});
+
