7802b82882905f5c2a37f5d7178839ca7d43aafb
Umar Thariq <umar.thariq@gdn-commerce.com>
2018-08-28 19:02:12 +0530

    added another article regarding Array

--- scripts/articles.js
+++ scripts/articles.js
@@ -6,15 +6,17 @@ var articles = [{
         desc: 'Basics of Angular v1.x has been clearly explained in this article'
     },{
         id: 2,
-        title:'Webpack 4.0 basic setup',
-        linkTitle:'Webpack-4.0-basic-setup',
-        banner_img: './images/webpack_1.png',
-        desc:'How to setup webpack 4 for your projects and make that one as development server.'
+        title:'Find an element in Array',
+        linkTitle:'javascript-find-element-in-array',
+        banner_img: '',
+        banner_title: 'Array',
+        desc:'Learn how to find an element in array in effective way with different versions of javascript'
     },{
         id: 3,
         title:'Singleton in Javascript',
         linkTitle:'singleton-in-javascript',
-        banner_img: './images/Singletons.png',
+        banner_img: '',
+        banner_title: 'Singleton',
         desc:'Understand the singleton pattern in Javascript'
     }
 ];
