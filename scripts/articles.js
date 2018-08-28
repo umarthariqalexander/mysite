6752094408aa4025277a7513da90cc39d6e68fbf
Umar Thariq <umar.thariq@gdn-commerce.com>
2018-08-28 15:30:35 +0530

    added singleton article

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,7 +1,7 @@
 var articles = [{
         id: 1,
         title: 'AngularJS v1.x basics',
-        linkTitle:'AngularJS-v1.x-basics',
+        linkTitle:'angularJS-v1.x-basics',
         banner_img: './images/angularjs_logo.png',
         desc: 'Basics of Angular v1.x has been clearly explained in this article'
     },{
@@ -12,10 +12,10 @@ var articles = [{
         desc:'How to setup webpack 4 for your projects and make that one as development server.'
     },{
         id: 3,
-        title:'Singletons in Javascript',
-        linkTitle:'Redux-basic-course',
+        title:'Singleton in Javascript',
+        linkTitle:'singleton-in-javascript',
         banner_img: './images/Singletons.png',
-        desc:'Understand the singletons pattern in Javascript'
+        desc:'Understand the singleton pattern in Javascript'
     }
 ];
 
