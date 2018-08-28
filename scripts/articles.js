cb5518ab2d5ba2490eee73c3ae0ce50a4e0989b5
Umar Thariq <umar.thariq@gdn-commerce.com>
2018-08-28 13:31:16 +0530

    singleton image added

--- scripts/articles.js
+++ scripts/articles.js
@@ -12,21 +12,10 @@ var articles = [{
         desc:'How to setup webpack 4 for your projects and make that one as development server.'
     },{
         id: 3,
-        title:'Redux basic course',
+        title:'Singletons in Javascript',
         linkTitle:'Redux-basic-course',
-        banner_img: './images/redux_logo.png',
-        desc:'Setup data management in React Projects with Redux'
-    },{
-        id: 4,
-        title: 'AngularJS v1.x basics',
-        linkTitle:'AngularJS-v1.x-basics',
-        banner_img: './images/angularjs_logo.png',
-        desc: 'Basics of Angular v1.x has been clearly explained in this article'
-    },{
-        id: 5,
-        title: 'AngularJS v1.x basics',
-        linkTitle:'AngularJS-v1.x-basics',
-        banner_img: './images/angularjs_logo.png' 
+        banner_img: './images/Singletons.png',
+        desc:'Understand the singletons pattern in Javascript'
     }
 ];
 
