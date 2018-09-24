4fbda0f5e917e2135fb3b1a202704b35715642ab
Umar Thariq <umar.thariq@gdn-commerce.com>
2018-09-24 16:08:06 +0530

    window.scroll top functionality added

--- scripts/index.js
+++ scripts/index.js
@@ -2,38 +2,41 @@ import {projectList} from './data.js';
 import '../styles/main.scss';
 import mainCtrl from './mainController.js';
 
-var app = angular.module('myApp', ["ngRoute"]);
+let app = angular.module('myApp', ['ngRoute']);
 app.controller('mainCtrl', ['$scope', '$location', '$http', mainCtrl]);
 
-app.config(['$routeProvider', function($routeProvider){
+app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
-  .when("/", {
-    templateUrl: "./views/articles.htm"
+  .when('/', {
+    templateUrl: './views/articles.htm'
   })
-  // .when("/article", {
-  //   templateUrl: "./views/article-view.htm"
-  // })
   .when('/article/:articleName*', {
-    templateUrl: function(urlattr){
+    templateUrl: function(urlattr) {
         return '/views/article-views/' + urlattr.articleName + '.htm';
-    },
+    }
   }
   )
-  .when("/projects", {
-    templateUrl: "./views/projectdesc.htm"
+  .when('/projects', {
+    templateUrl: './views/projectdesc.htm'
   })
-  .when("/tech", {
-    templateUrl: "./views/technologydesc.htm"
+  .when('/tech', {
+    templateUrl: './views/technologydesc.htm'
   })
-  .when("/about", {
-    templateUrl: "./views/about.htm"
+  .when('/about', {
+    templateUrl: './views/about.htm'
   })
-  .when("/contact", {
-    templateUrl: "./views/contact.htm"
-  })
+  .when('/contact', {
+    templateUrl: './views/contact.htm'
+  });
 }]);
 
-  
+app.run(function ($rootScope) {
+  $rootScope.$on('$locationChangeSuccess', function () {
+      window.scroll(0, 0);
+  });
+});
+
+
 // var scrollUpEvent = window.addEventListener('scroll',function(){
 //     if(window.pageYOffset>350){
 //       document.getElementById('scrollUpIcon').classList.add('display-inline-block');
@@ -42,4 +45,4 @@ app.config(['$routeProvider', function($routeProvider){
 //       document.getElementById('scrollUpIcon').classList.remove('display-inline-block');
 //     }
 // });
-  
+
