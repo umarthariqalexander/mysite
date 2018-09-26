1c7eabd13924867e1d71a62973915b9305df323c
Umar Thariq <umar.thariq@gdn-commerce.com>
2018-09-26 16:42:39 +0530

    new article coercion added

--- scripts/articles.js
+++ scripts/articles.js
@@ -6,40 +6,45 @@ let articles = [{
         desc: 'Basics of Angular v1.x has been clearly explained in this article'
     }, {
         id: 2,
-        title: 'Find an element in Array',
-        linkTitle: 'javascript-find-element-in-array',
+        title: 'Coercion in JavaScript',
+        linkTitle: 'javascript-coercion',
         banner_img: '',
-        banner_title: 'Array',
-        desc: 'Learn how to find an element in array in effective way with different versions of javascript'
+        banner_title: 'Coercion',
+        desc: 'Learn type coercion of JavaScript in detailed manner'
     }, {
-        id: 3,
+        id: 5,
+        title: 'Object Enumeration',
+        linkTitle: 'javascript-object-enumeration',
+        banner_img: '',
+        banner_title: 'Enumeration',
+        desc: 'Learn different ways to iterate/enumerate key and values of an Object'
+    }, {
+        id: 4,
         title: 'Singleton in JavaScript',
         linkTitle: 'singleton-in-javascript',
         banner_img: '',
         banner_title: 'Singleton',
         desc: 'Understand the singleton pattern in Javascript'
     }, {
-        id: 4,
-        title: 'Object Enumeration',
-        linkTitle: 'javascript-object-enumeration',
+        id: 3,
+        title: 'Find an element in Array',
+        linkTitle: 'javascript-find-element-in-array',
         banner_img: '',
-        banner_title: 'Enumeration',
-        desc: 'Learn different ways to iterate/enumerate key and values of an Object'
-    },
-    {
-        id: 5,
+        banner_title: 'Array',
+        desc: 'Learn how to find an element in array in effective way with different versions of javascript'
+    }, {
+        id: 6,
         title: 'Set in Javascript',
         linkTitle: 'javascript-set',
         banner_img: '',
         banner_title: 'Set',
         desc: 'Learn new ES6 Data structure Set'
-    },
-    {
-        id: 6,
+    }, {
+        id: 7,
         title: 'Remove duplicates - Array',
         linkTitle: 'remove-duplicates-in-array-javascript',
         banner_img: '',
-        banner_title: 'Array',
+        banner_title: 'Duplicates',
         desc: 'Learn efficient & easy way to remove duplicates of Array using Set data structure'
     }
 ];
