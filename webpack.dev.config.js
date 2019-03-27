762762024030a8ebfc174d6f8ad3d0bcc0336fd0
Umar Thariq <umar.thariq@gdn-commerce.com>
2019-03-27 15:43:06 +0530

    created different files for production and development builds

---
+++ webpack.dev.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            { 
+                test: /\.jsx?$/, 
+                loader: 'babel-loader', 
+                exclude: /node_modules/ 
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|mp3)$/i,
+                use: ['file-loader']
+            },
+            {
+                test: /\.svg$/,
+                loader: 'url-loader'
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "dist")
+    }
+};
+
