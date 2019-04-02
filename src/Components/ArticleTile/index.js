0a991ee9e69532477c1798250b611e502abf4e5c
Umar Thariq <umar.thariq@gdn-commerce.com>
2019-04-02 15:08:43 +0530

    image configuration added for webpack

--- src/Components/ArticleTile/index.js
+++ src/Components/ArticleTile/index.js
@@ -5,7 +5,7 @@ import './styles.scss';
 const ArticleTile = ({article, onClick})=>{
     return(
         <div className="article-tile-wrapper" onClick={onClick}>
-            <img className="article-image" src={article.imageUrl} />
+            <img className="article-image" src={require(`../../images/${article.bannerImg}`)} />
             <div className="article-heading-wrapper">
                 <span className="article-heading">{article.heading}</span>
             </div>
--- src/static/articles.js
+++ src/static/articles.js
@@ -1,31 +1,22 @@
 export default [
     {
         heading: 'React Hooks explained with relevant examples', 
-        imageUrl:'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F54866664%2F206967069597%2F1%2Foriginal.20190109-161005?auto=compress&s=56211b98337cb82bd9aeb304fa5ecd0e'}, 
-    {
-        heading: 'React Articture', 
-        imageUrl: 'https://cdn-images-1.medium.com/max/1600/1*OF0xEMkWBv-69zvmNs6RDQ.gif'
+        bannerImg:'React_Hooks.png'
     },
     {
-        heading: 'React Hooks', 
-        imageUrl:'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F54866664%2F206967069597%2F1%2Foriginal.20190109-161005?auto=compress&s=56211b98337cb82bd9aeb304fa5ecd0e'}, 
-    {
-        heading: 'React Articture', 
-        imageUrl: 'https://cdn-images-1.medium.com/max/1600/1*OF0xEMkWBv-69zvmNs6RDQ.gif'
+        heading: 'React Hooks explained with relevant examples', 
+        bannerImg:'Default_JS.jpg'
     },
     {
-        heading: 'React Hooks', 
-        imageUrl:'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F54866664%2F206967069597%2F1%2Foriginal.20190109-161005?auto=compress&s=56211b98337cb82bd9aeb304fa5ecd0e'}, 
-    {
-        heading: 'React Articture', 
-        imageUrl: 'https://cdn-images-1.medium.com/max/1600/1*OF0xEMkWBv-69zvmNs6RDQ.gif'
+        heading: 'React Hooks explained with relevant examples', 
+        bannerImg:'React_Hooks.png'
     },
     {
-        heading: 'React Hooks', 
-        imageUrl:'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F54866664%2F206967069597%2F1%2Foriginal.20190109-161005?auto=compress&s=56211b98337cb82bd9aeb304fa5ecd0e'}, 
+        heading: 'React Hooks explained with relevant examples', 
+        bannerImg:'React_Hooks.png'
+    },
     {
-        heading: 'React Articture', 
-        imageUrl: 'https://cdn-images-1.medium.com/max/1600/1*OF0xEMkWBv-69zvmNs6RDQ.gif'
+        heading: 'React Hooks explained with relevant examples', 
+        bannerImg:'React_Hooks.png'
     }
 ];
-
